1f817772395df61b784edca3b9b302d50f84f9ea
AKASH <kumarakash11072002@gmail.com>
2023-08-05 01:02:47 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -21,7 +21,7 @@ const getDataFromDB = require('./db');
   const app = express()
   const port = 5000
   app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "https://news-nexus-mern.netlify.app");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
